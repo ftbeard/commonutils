0c01ae09d2d8672eb5b5b71e1e629627f8c4ad5a
ato42 <jvincent@student.42.fr>
2016-02-03 20:07:57 +0100

    Update commonutils.rb

--- commonutils.rb
+++ commonutils.rb
@@ -3,7 +3,7 @@ class Commonutils < Formula
   homepage ""
   url "https://github.com/ftbeard/commonutils/archive/0.0.1.tar.gz"
   version "0.0.1"
-  sha256 "b59acc73206118131db469b06e9ec9c115e73bfb0e49f870ea1754c739878268"
+  sha256 "a4ae20fb884a3b403f04e25ae522a695d5332ac0fb68fadb16a30cd05dd4b1a1"
 
   def install
 	ENV.deparallelize
