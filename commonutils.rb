bf11f9dcbec9605957adf303ab65c7c92f985f41
Jeremy VINCENT <jvincent@e1r5p14.42.fr>
2016-02-03 16:17:51 +0100

    modify the sha-256

--- commonutils.rb
+++ commonutils.rb
@@ -3,7 +3,7 @@ class Commonutils < Formula
   homepage ""
   url "https://github.com/ftbeard/commonutils/archive/0.0.1.tar.gz"
   version "0.0.1"
-  sha256 ""
+  sha256 "bfcf91486443063fbf4c2f295e3ca58221cbc368dd8b6624f278eb09a6cab42d"
 
   depends_on "lua51"
 
