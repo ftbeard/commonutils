5b7a0b13792e1bc1050b96088968e3e2114dd730
ato42 <jvincent@student.42.fr>
2016-02-03 16:08:00 +0100

    Update commonutils.rb

--- commonutils.rb
+++ commonutils.rb
@@ -1,8 +1,8 @@
 class Commonutils < Formula
   desc "ftbeard commons utilities"
   homepage ""
-  url "https://github.com/ftbeard/commonutils/archive/0.0.1.tar.gz"
-  version "0.0.1"
+  url "https://github.com/ftbeard/commonutils/archive/0.0.2.tar.gz"
+  version "0.0.2"
   sha256 "e09bf04b2afc7c9c039520797c216c569c8ede22f4fd573035e61a93dd58597a"
 
   depends_on "lua51"
