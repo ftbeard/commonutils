be87e50c220e3aee60efd85bf171e857fb0d5826
ato42 <jvincent@student.42.fr>
2016-02-03 16:09:08 +0100

    Update commonutils.rb

--- commonutils.rb
+++ commonutils.rb
@@ -3,7 +3,7 @@ class Commonutils < Formula
   homepage ""
   url "https://github.com/ftbeard/commonutils/archive/0.0.2.tar.gz"
   version "0.0.2"
-  sha256 "e09bf04b2afc7c9c039520797c216c569c8ede22f4fd573035e61a93dd58597a"
+  sha256 ""
 
   depends_on "lua51"
 
