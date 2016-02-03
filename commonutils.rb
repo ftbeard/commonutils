8f06e167ccbf8245cf5e8db67c1d4b565800de1e
ato <ato@Vault.local>
2016-02-03 20:29:47 +0100

    hmm hmm

--- commonutils.rb
+++ commonutils.rb
@@ -3,15 +3,15 @@ class Commonutils < Formula
   homepage ""
   url "https://github.com/ftbeard/commonutils/archive/0.0.1.tar.gz"
   version "0.0.1"
-  sha256 "ce2f58761f690eba6bf3a882440f9386542dcee4a651a749cb9a4d0c4b301d53"
+  sha256 "071d84bccb0ff2214567327c3ff811d2ff2785642d1bb35486c13e1884288908"
 
   def install
-	ENV.deparallelize
-	bin.install "bin/confirm"
+    ENV.deparallelize  # if your formula fails when building in parallel
 
-	system "make"
-	system "make", "install"
+    system "make"
+    system "make", "install" # if this fails, try separate make/make install steps
 
+    bin.install "bin/confirm"
   end
 
 end
