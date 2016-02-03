1c6cab2d26c57f507e349111b2693dce1c304d39
ato <ato@Vault.local>
2016-02-03 18:39:22 +0100

    add formula

---
+++ commonutils.rb
@@ -0,0 +1,11 @@
+class Commonutils < Formula
+  desc ""
+  homepage ""
+  url "https://github.com/ftbeard/commonutils/archive/0.0.1.tar.gz"
+  version "0.0.1"
+  sha256 "b59acc73206118131db469b06e9ec9c115e73bfb0e49f870ea1754c739878268"
+
+  def install
+  end
+
+end
