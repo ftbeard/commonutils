306554a5bc42636e40fbba47187767fbae1610da
ato <ato@Vault.local>
2016-02-04 10:16:57 +0100

    let's try

--- commonutils.rb
+++ commonutils.rb
@@ -14,6 +14,8 @@ class Commonutils < Formula
     bin.install "bin/confirm"
 
     lib.install "lib/moon/utils.moon"
+    system bin/"luarocks-5.1", "install", "moonscript", "--tree=#{testpath}/luarocks"
+
   end
 
 end
